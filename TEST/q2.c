8d5388c389773e48e597cd35ee120e3dffb79b84
himanshu24 <himanshu24022002@gmail.com>
2023-11-18 23:44:21 +0530

    DAA LAB

--- TEST/q2.c
+++ TEST/q2.c
@@ -1,91 +1,84 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-#define MAX_VERTICES 10 // Number of vertices in the graph (1 to 10)
-
-// Function to calculate the weight of an edge between vertices i and j
-int calculateWeight(int i, int j) {
-    return ((i + j) * (i + j)) % 100;
+#include <stdlib.h>
+#define MAX 10
+struct Edge {
+    int src_2252, dest_2252, weight_2252;
+};
+int find_2252(int parent_2252[], int i_2252) {
+    if (parent_2252[i_2252] == -1)
+        return i_2252;
+    return find_2252(parent_2252, parent_2252[i_2252]);
 }
 
-// Function to find the vertex with the minimum key value
-int findMinKey(int key[], bool mstSet[]) {
-    int min = 101, min_index;
-
-    for (int v = 1; v <= MAX_VERTICES; v++) {
-        if (mstSet[v] == false && key[v] < min) {
-            min = key[v];
-            min_index = v;
-        }
-    }
-
-    return min_index;
+void unionSets_2252(int parent_2252[], int x_2252, int y_2252) {
+    int xroot_2252 = find_2252(parent_2252, x_2252);
+    int yroot_2252 = find_2252(parent_2252, y_2252);
+    parent_2252[xroot_2252] = yroot_2252;
 }
 
-// Function to print the cost-adjacency list representation of the minimum-cost spanning tree
-void printMinimumCostSpanningTree(int parent[], int graph[MAX_VERTICES + 1][MAX_VERTICES + 1]) {
-    printf("Minimum-Cost Spanning Tree (Cost-Adjacency List Representation):\n");
-    printf("Edge \t\tWeight\n");
-    for (int i = 2; i <= MAX_VERTICES; i++) {
-        printf("%d - %d \t\t%d\n", parent[i], i, graph[i][parent[i]]);
-    }
+int compareEdges_2252(const void *a_2252, const void *b_2252) {
+    return ((struct Edge *)a_2252)->weight_2252 - ((struct Edge *)b_2252)->weight_2252;
 }
 
-// Function to perform Prim's algorithm to find the minimum-cost spanning tree
-void primMST(int graph[MAX_VERTICES + 1][MAX_VERTICES + 1]) {
-    int parent[MAX_VERTICES + 1]; // Array to store constructed MST
-    int key[MAX_VERTICES + 1];    // Key values used to pick minimum weight edge
-    bool mstSet[MAX_VERTICES + 1]; // To represent set of vertices included in MST
+void kruskalMST_2252(int cost_2252[MAX][MAX]) {
+    struct Edge result_2252[MAX];
+    int e_2252 = 0;
 
-    // Initialize all keys as infinite
-    for (int i = 1; i <= MAX_VERTICES; i++) {
-        key[i] = 101; // Initialize with a value greater than any possible edge weight (as per given constraints)
-        mstSet[i] = false;
-    }
-
-    // Always include vertex 1 as the first vertex
-    key[1] = 0;
-    parent[1] = -1; // No parent of the first node
-
-    // Construct the MST with MAX_VERTICES - 1 vertices
-    for (int count = 1; count <= MAX_VERTICES - 1; count++) {
-        int u = findMinKey(key, mstSet);
-        mstSet[u] = true;
-
-        // Update key values of adjacent vertices of the picked vertex
-        for (int v = 1; v <= MAX_VERTICES; v++) {
-            // Update key[v] only if it is not in the MST, there is an edge from u to v,
-            // and the weight of the edge is smaller than the current key value of v
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = graph[u][v];
+    struct Edge edges_2252[MAX * MAX];
+    int edgeCount_2252 = 0;
+    for (int i_2252 = 0; i_2252 < MAX; i_2252++) {
+        for (int j_2252 = i_2252 + 1; j_2252 < MAX; j_2252++) {
+            if (cost_2252[i_2252][j_2252] != 0) {
+                edges_2252[edgeCount_2252].src_2252 = i_2252;
+                edges_2252[edgeCount_2252].dest_2252 = j_2252;
+                edges_2252[edgeCount_2252].weight_2252 = cost_2252[i_2252][j_2252];
+                edgeCount_2252++;
             }
         }
     }
+    qsort(edges_2252, edgeCount_2252, sizeof(struct Edge), compareEdges_2252);
 
-    // Print the minimum-cost spanning tree
-    printMinimumCostSpanningTree(parent, graph);
+    int parent_2252[MAX];
+    for (int i_2252 = 0; i_2252 < MAX; i_2252++)
+        parent_2252[i_2252] = -1;
+
+    printf("Edge Cost\n");
+    for (int i_2252 = 0; i_2252 < edgeCount_2252; i_2252++) {
+        int x_2252 = find_2252(parent_2252, edges_2252[i_2252].src_2252);
+        int y_2252 = find_2252(parent_2252, edges_2252[i_2252].dest_2252);
+
+        if (x_2252 != y_2252) {
+            result_2252[e_2252++] = edges_2252[i_2252];
+            unionSets_2252(parent_2252, x_2252, y_2252);
+        }
+    }
+    for (int i_2252 = 0; i_2252 < e_2252; i_2252++) {
+        printf("(%d, %d) %d\n", result_2252[i_2252].src_2252 + 1, result_2252[i_2252].dest_2252 + 1, result_2252[i_2252].weight_2252);
+    }
 }
 
 int main() {
-    // Create the cost-adjacency matrix representation of the graph
-    int graph[MAX_VERTICES + 1][MAX_VERTICES + 1];
-
-    // Initialize the graph with edge weights
-    for (int i = 1; i <= MAX_VERTICES; i++) {
-        for (int j = 1; j <= MAX_VERTICES; j++) {
-            if ((i < j && j % i == 0) || (j == i + 1)) {
-                graph[i][j] = calculateWeight(i, j);
-                graph[j][i] = calculateWeight(i, j); // Undirected graph
+    int n_2252 = 10;
+    int cost_2252[MAX][MAX];
+    for (int i_2252 = 0; i_2252 < n_2252; i_2252++) {
+        for (int j_2252 = 0; j_2252 < n_2252; j_2252++) {
+            if ((j_2252 == i_2252 + 1) || (i_2252 > 0 && j_2252 % i_2252 == 0)) {
+                cost_2252[i_2252][j_2252] = (i_2252 + j_2252) * (i_2252 + j_2252) % 100;
+                cost_2252[j_2252][i_2252] = cost_2252[i_2252][j_2252];  
             } else {
-                graph[i][j] = 0; // No edge between vertices i and j
+                cost_2252[i_2252][j_2252] = 0; 
             }
         }
     }
-
-    // Perform Prim's algorithm to find the minimum-cost spanning tree
-    printf("Finding Minimum-Cost Spanning Tree using Prim's Algorithm:\n");
-    primMST(graph);
+    printf("Cost-adjacency matrix:\n");
+    for (int i_2252 = 0; i_2252 < n_2252; i_2252++) {
+        for (int j_2252 = 0; j_2252 < n_2252; j_2252++) {
+            printf("%4d ", cost_2252[i_2252][j_2252]);
+        }
+        printf("\n");
+    }
+    printf("\nMinimum-cost spanning tree (Kruskal's algorithm): \n");
+    kruskalMST_2252(cost_2252);
 
     return 0;
 }
